7704829bda9d3082b5f17421ded57408b3ff8ae7
Adam Schubert <adam.schubert@sg1-game.net>
2016-02-04 04:24:17 +0100

    Generate mo file when generating PO file

--- tools/resx2po.py
+++ tools/resx2po.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+
 from resx2dict import resx2dict
 import os
 import polib
 import shutil
 
 class resx2po(object):
-    def __init__(self, resxen2name, resxlocale, potemplate, out):
+    """
+    Converts resx to po and mo, it is using template.po file to keep file.py:line_number info
+    """
+    def __init__(self, resxen2name, resxlocale, potemplate, out_path, code):
         if os.path.isfile(resxen2name) is False:
             raise Exception("Resx bound not found")
 
@@ -25,6 +30,19 @@ class resx2po(object):
                 print("WARNING: {} not found in {}, wont be added into motable".format(boundk, resxen2name))
 
         po = polib.pofile(potemplate)
+
+        po.metadata = {
+            'Project-Id-Version': '1.0',
+            'Report-Msgid-Bugs-To': 'adam.schubert@sg1-game.net',
+            'POT-Creation-Date': '2016-01-19 02:00+0100',
+            'PO-Revision-Date': '2016-01-19 02:00+0100',
+            'Last-Translator': 'Adam Schubert <adam.schubert@sg1-game.net>',
+            'Language-Team': '',
+            'MIME-Version': '1.0',
+            'Content-Type': 'text/plain; charset=utf-8',
+            'Content-Transfer-Encoding': '8bit',
+            'Language': code
+        }
         for entry in po:
             if entry.msgid in motable:
                 entry.msgstr = motable[entry.msgid]
@@ -32,18 +50,19 @@ class resx2po(object):
                 print("WARNING: {} not found in {}".format(entry.msgid, 'motable'))
 
         po.save()
-        shutil.copy2(potemplate, out)
+        po.save_as_mofile(os.path.join(out_path, '{}.mo'.format(code)))
+        shutil.copy2(potemplate, os.path.join(out_path, '{}.po'.format(code)))
 
 
 directory = '../locale'
-resx2po('Resources.resx', 'Resources.de.resx', 'messages.po', os.path.join(directory, 'de_DE.po'))
-resx2po('Resources.resx', 'Resources.es.resx', 'messages.po', os.path.join(directory, 'es_ES.po'))
-resx2po('Resources.resx', 'Resources.fr.resx', 'messages.po', os.path.join(directory, 'fr_FR.po'))
-resx2po('Resources.resx', 'Resources.it.resx', 'messages.po', os.path.join(directory, 'it_IT.po'))
-resx2po('Resources.resx', 'Resources.nl.resx', 'messages.po', os.path.join(directory, 'nl_NL.po'))
-resx2po('Resources.resx', 'Resources.no.resx', 'messages.po', os.path.join(directory, 'nb_NO.po'))
-resx2po('Resources.resx', 'Resources.pt.resx', 'messages.po', os.path.join(directory, 'pt_PT.po'))
-resx2po('Resources.resx', 'Resources.ru.resx', 'messages.po', os.path.join(directory, 'ru_RU.po'))
-resx2po('Resources.resx', 'Resources.tr.resx', 'messages.po', os.path.join(directory, 'tr_TR.po'))
-resx2po('Resources.resx', 'Resources.uk.resx', 'messages.po', os.path.join(directory, 'uk_UA.po'))
-resx2po('Resources.resx', 'Resources.zh-CHS.resx', 'messages.po', os.path.join(directory, 'zh_CN.po'))
+resx2po('Resources.resx', 'Resources.de.resx', 'messages.po', directory, 'de_DE')
+resx2po('Resources.resx', 'Resources.es.resx', 'messages.po', directory, 'es_ES')
+resx2po('Resources.resx', 'Resources.fr.resx', 'messages.po', directory, 'fr_FR')
+resx2po('Resources.resx', 'Resources.it.resx', 'messages.po', directory, 'it_IT')
+resx2po('Resources.resx', 'Resources.nl.resx', 'messages.po', directory, 'nl_NL')
+resx2po('Resources.resx', 'Resources.no.resx', 'messages.po', directory, 'nb_NO')
+resx2po('Resources.resx', 'Resources.pt.resx', 'messages.po', directory, 'pt_PT')
+resx2po('Resources.resx', 'Resources.ru.resx', 'messages.po', directory, 'ru_RU')
+resx2po('Resources.resx', 'Resources.tr.resx', 'messages.po', directory, 'tr_TR')
+resx2po('Resources.resx', 'Resources.uk.resx', 'messages.po', directory, 'uk_UA')
+resx2po('Resources.resx', 'Resources.zh-CHS.resx', 'messages.po', directory, 'zh_CN')
