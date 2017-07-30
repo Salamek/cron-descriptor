e104d29b0131dc2e24f2b1d5145336cae013371d
Adam Schubert <adam.schubert@sg1-game.net>
2017-07-30 16:03:24 +0200

    Python :: 3.5

--- setup.py
+++ setup.py
@@ -48,6 +48,8 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.2",
             "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
             "Topic :: Software Development",
         ],
         test_suite="tests"
