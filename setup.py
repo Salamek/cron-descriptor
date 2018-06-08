e9bfacd841db56aa435564fad571176643d09c5e
Adam Schubert <adam.schubert@sg1-game.net>
2018-06-08 15:29:13 +0200

    New version 1.2.13

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     
     setuptools.setup(
         name="cron_descriptor",
-        version="1.2.12",
+        version="1.2.13",
         description="A Python library that converts cron expressions "
                     "into human readable strings.",
         author="Adam Schubert",
