0753acb3a83e8bc92b3c752b03e89cd5ef25809d
Adam Schubert <adam.schubert@sg1-game.net>
2024-06-25 20:10:38 +0200

    chore(setup.py): Update classifiers

--- setup.py
+++ setup.py
@@ -46,15 +46,16 @@ if __name__ == "__main__":
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Environment :: Web Environment",
+            "Environment :: Console",
             "Intended Audience :: Developers",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Programming Language :: Python",
-            "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Topic :: Software Development",
         ],
         extras_require={
