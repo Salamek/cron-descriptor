54a14b28915fcfb8fde277d62cccf96e0751d669
Adam Schubert <adam.schubert@sg1-game.net>
2025-08-25 19:30:31 +0200

    feat(Test): added test_verbosity test

---
+++ tests/test_verbosity.py
@@ -0,0 +1,40 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Adam Schubert
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from cron_descriptor import get_description, Options
+
+"""
+Tests verbosity
+"""
+
+def test_simple_expression(options: Options) -> None:
+    options.verbose = True
+    assert get_description("30 4 1 * *", options) == "At 04:30 AM, on day 1 of the month"
+
+
+def test_every_minute_simple_expression(options: Options) -> None:
+    options.verbose = True
+    assert get_description("* * * * *", options) == "Every minute, every hour, every day"
+
+def test_single_day_of_the_week(options: Options) -> None:
+    options.verbose = True
+    assert get_description("0 9 * * 2", options) == "At 09:00 AM, only on Tuesday"
