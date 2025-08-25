54947f5e2a8fa2e01585d69db91c89da8987ccc9
Adam Schubert <adam.schubert@sg1-game.net>
2025-08-25 23:39:39 +0200

    New version 2.0.1

--- cron_descriptor/__init__.py
+++ cron_descriptor/__init__.py
@@ -26,7 +26,7 @@ from .Exception import FormatError, MissingFieldError, WrongArgumentError
 from .ExpressionDescriptor import ExpressionDescriptor, get_description
 from .Options import Options
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __all__ = [
            "CasingTypeEnum",
            "DescriptionTypeEnum",
