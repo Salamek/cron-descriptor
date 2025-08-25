6c45e17aefdab668a547336b0ef528af6702c6b3
Adam Schubert <adam.schubert@sg1-game.net>
2025-08-25 21:35:58 +0200

    chore: Lint

--- tools/compilepos.py
+++ tools/compilepos.py
@@ -1,10 +1,13 @@
-import polib
+import logging
 import pathlib
 
+import polib
+
+log = logging.getLogger(__name__)
 base_dir = pathlib.Path("../cron_descriptor/locale")
 
 for po_file in base_dir.rglob("*.po"):
     mo_file = po_file.with_suffix(".mo")
-    print(f"Compiling {po_file} → {mo_file}")
+    log.info("Compiling %s → %s", po_file, mo_file)
     po = polib.pofile(po_file)
     po.save_as_mofile(str(mo_file))
