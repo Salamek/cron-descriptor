9dab5aca56313cb96236e6be9082c1e3486f1b67
Adam Schubert <adam.schubert@sg1-game.net>
2025-08-25 19:49:36 +0200

    feat(CI): Don't call setup.py except for stdeb

--- pre-commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-find cron_descriptor tests examples -name '*.py' -exec autopep8 --in-place --max-line-length=120 '{}' \;
-./code-check.sh
-python setup.py test
-python3 setup.py test
